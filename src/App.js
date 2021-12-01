7cbbce435f081e658b5acc933efa9e4e88a54fb7
joseph-vega12 <57374454+joseph-vega12@users.noreply.github.com>
2021-11-30 22:38:03 -0800

    basic sidebarNav added with basic template

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
+import SidebarNav from "./components/common/SidebarNav";
 function App() {
   return (
     <div>
-      <h1>Admin Panel</h1>
       {/*      SideBar Navigation      */}
-            {/*    Live Orders     */}
-            {/*   Order History    */}
-            {/*     Products       */}
-            {/*     Settings       */}
+          {/*    Live Orders     */}
+          {/*   Order History    */}
+          {/*     Products       */}
+          {/*     Settings       */}
       {/*      SideBar Navigation      */}
+      <SidebarNav/>
       
       {/*  Components */}
       {/*  */}
---
+++ src/components/common/SidebarNav.js
@@ -0,0 +1,47 @@
+import { Drawer, List, ListItem, ListItemText } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import "./SidebarNav.css";
+
+function SidebarNav() {
+  const SelectedItemList = styled(ListItem)(() => ({
+    backgroundColor: "#FFEBEC",
+    borderRadius: "10px",
+    ".MuiTypography-root": {
+      color: "#FF3E40",
+      borderRadius: "10px",
+    },
+  }));
+
+  const ItemList = styled(ListItem)(() => ({
+    borderRadius: "10px",
+    "&:hover": {
+      backgroundColor: "#FFEBEC",
+      borderRadius: "10px",
+    },
+  }));
+
+  return (
+    <nav>
+      <Drawer
+        open={true}
+        // variant="permanent"
+        ModalProps={{
+          keepMounted: true,
+        }}
+      >
+        <List>
+          <SelectedItemList button>
+            <ListItemText primary={"Live Orders"} />
+          </SelectedItemList>
+          {["Order History", "Products", "Settings"].map((text, index) => (
+            <ItemList button key={text}>
+              <ListItemText primary={text} />
+            </ItemList>
+          ))}
+        </List>
+      </Drawer>
+    </nav>
+  );
+}
+
+export default SidebarNav;
